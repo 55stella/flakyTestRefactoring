a015c64b8acacf5b2199ea04c5f2a313a848dae6
55stella <stellarichards7@gmail.com>
2025-01-04 17:49:16 +0100

    refactored pagebase

--- test/pageobjects/pageBase.page.js
+++ test/pageobjects/pageBase.page.js
@@ -5,8 +5,8 @@ export default class PageBase {
   async click(webElement) {
     await webElement.click();
   }
-  async sendText(webElement) {
-    await webElement.sendText();
+  async sendText(webElement, text) {
+    await webElement.sendText(text);
   }
   async getText(webElement) {
     await webElement.getText();
